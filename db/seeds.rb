abe547dae44de505fc62444755b721f0c630dc43
Zara Peisker <zarapeisker@gmail.com>
2013-06-14 11:45:21 -0700

    refactored seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,26 +9,14 @@
 
 require "open-uri"
 
-@bsams_raw = Nokogiri::HTML(open("http://www.simplyrecipes.com/recipes/baked_salmon_with_avocado_mango_salsa/"))
-@bsams_name = @bsams_raw.css("h1.entry-title")[0].content
-@bsams_content = @bsams_raw.css(".recipe-description p")[0].content
-@bsams_recipe = Recipe.create(:name => @bsams_name, :content => @bsams_content)
+recipes = [baked_salmon_with_avocado_mango_salsa, mediterranean_chicken_salad, shrimp_and_grits, beef_brisket_pot_roast]
 
-@mcs_raw = Nokogiri::HTML(open("http://www.simplyrecipes.com/recipes/mediterranean_chicken_salad/"))
-@mcs_name = @mcs_raw.css("h1.entry-title")[0].content
-@mcs_content = @mcs_raw.css(".recipe-description p")[0].content
-@mcs_recipe = Recipe.create(:name => @mcs_name, :content => @mcs_content)
-
-@sg_raw = Nokogiri::HTML(open("http://www.simplyrecipes.com/recipes/shrimp_and_grits/"))
-@sg_name = @sg_raw.css("h1.entry-title")[0].content
-@sg_content = @sg_raw.css(".recipe-description p")[0].content
-@sg_recipe = Recipe.create(:name => @sg_name, :content => @sg_content)
-
-@bbpr_raw = Nokogiri::HTML(open("http://www.simplyrecipes.com/recipes/beef_brisket_pot_roast/"))
-@bbpr_name = @bbpr_raw.css("h1.entry-title")[0].content
-@bbpr_content = @bbpr_raw.css(".recipe-description p")[0].content
-@bbpr_recipe = Recipe.create(:name => @bbpr_name, :content => @bbpr_content)
-
+recipes.each do |recipe|
+  recipe = Nokogiri::HTML(open("http://www.simplyrecipes.com/recipes/#{recipe}/"))
+  title = recipe.css("h1.entry-title")[0].content
+  description = recipe.css(".recipe-description p")[0].content
+  Recipe.create(:name => title, :content => description)
+end
 
 @twilio_client = Twilio::REST::Client.new(
   "gibberish",
